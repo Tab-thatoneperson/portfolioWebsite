292644d4364a7bac48a1d8c29a8e4b908d058bb3
Tabitha M <tabitha.d.morley@gmail.com>
2026-03-20 23:45:29 +1100

    contact form with emailjs

--- index.js
+++ index.js
@@ -301,3 +301,27 @@ function checkIntersection(goose, element, pos) {
             };
     }
 };
+
+//------------------------------------CONTACT EMAIL JS-----------------------------//
+const contactForm = document.getElementById('contact-form'),
+    contactMessage = document.getElementById('contact-message')
+
+const sendEmail = (e) => {
+    e.preventDefault();
+
+    //serviceID - templateID - #form - publicKey
+    emailjs.sendForm('service_whn0bcy', 'template_4li170n', '#contact-form', 'trUEz8AEGk4I4VtNM')
+    .then(() => {
+        contactMessage.textContent = "Message sent successfully!";
+
+        setTimeout(() => {
+            contactMessage.textContent = ''
+        }, 5000)
+
+        contactForm.reset();
+    }, () => {
+        contactMessage.textContent = 'Message not sent (service error)'
+    })
+}
+
+contactForm.addEventListener('submit', sendEmail);
